1bd3130a8d4b0465cbbe6a266d95086c1ba88803
David Stewart <davidstewart@gmail.com>
2019-07-28 10:19:55 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +9,18 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+     
+  end
+
+  def create
+   @student = Student.new(first_name: params[:student][:first_name],last_name: params[:student][:last_name])
+   @student.save
+  
+   redirect_to new_student_path
+  end
+
+
 
 end
