48ffe65c9fa7e2ae0bb55186d72b7122b3d2574a
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-08-04 20:23:31 +0100

    create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,11 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    Student.create(first_name: params[:student][:first_name],second_name: params[:student][:second_name])
+    redirect_to student_path
+  end
+
   def new
   end
 
